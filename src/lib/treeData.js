4bacfc904b6d7838d97ea66ad30d019bb6dfb8af
SMAghazadeh <maghazadeh085@gmail.com>
2024-10-21 11:11:28 +0330

    Add Data for tree structure

---
+++ src/lib/treeData.js
@@ -0,0 +1,90 @@
+ export const treeData = [
+    {
+        key: 0,
+        label: 'Documents',
+        children: [
+            {
+                key: 0-0,
+                label: 'Adrian',
+                children: [
+                    {
+                        key: 0 - 0 - 0,
+                        label: 'Animation',
+                        children: []
+                    },
+                    {
+                        key: 0 - 0 - 1,
+                        label: 'nextjs',
+                        children: []
+                    }
+                ]
+            },
+            {
+                key: 0-1,
+                label: 'Css',
+                children: [
+                    {
+                        key:0-1-0,
+                        label: 'Tailwind css',
+                        children: []
+                    }
+                ]
+            },
+            {
+                key: 0-2,
+                label: 'HTML',
+                children: []
+            },
+            {
+                key: 0-3,
+                label: 'JavaScripts',
+                children: [
+                    {
+                        key:0-3-0,
+                        label: 'React',
+                        children: []
+                    },
+                    {
+                        key:0-3-1,
+                        label: 'Angular',
+                        children: []
+                    },
+                    {
+                        key:0-3-2,
+                        label: 'vue',
+                        children: []
+                    },
+                ]
+            },
+
+        ]
+    },
+    {
+        key: 1,
+        label: 'Downloads',
+        children: [
+            {
+                key: 1-0,
+                label: 'YouTube',
+                children: []
+            },
+            {
+                key: 1-1,
+                label: 'Instagram',
+                children: []
+            },
+            {
+                key: 1-2,
+                label: 'Twitter',
+                children: []
+            },
+            {
+                key: 0-3,
+                label: 'Telegram',
+                children: []
+            },
+
+        ]
+    },
+
+];
